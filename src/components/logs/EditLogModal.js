729dd82f6b1b7031047fb9f4ae599e6faac669eb
fepu08 <ferencbesenyei@protonmail.com>
2021-02-17 22:11:47 +0100

    clear log.current state after update log

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { updateLog } from '../../actions/logActions';
+import { updateLog, clearCurrent } from '../../actions/logActions';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
-const EditLogModal = ({ current, updateLog }) => {
+const EditLogModal = ({ current, updateLog, clearCurrent }) => {
   const [message, setMessage] = useState('');
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState('');
@@ -30,6 +30,7 @@ const EditLogModal = ({ current, updateLog }) => {
       };
       updateLog(updatedLog);
       clearFields();
+      clearCurrent();
       M.toast({ html: `Log updated by ${tech}` });
     }
   };
@@ -110,11 +111,14 @@ const modalStyle = {
 
 EditLogModal.propTypes = {
   current: PropTypes.object.isRequired,
-  updateLog: PropTypes.func.isRequired
+  updateLog: PropTypes.func.isRequired,
+  clearCurrent: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
   current: state.log.current
 });
 
-export default connect(mapStateToProps, { updateLog })(EditLogModal);
+export default connect(mapStateToProps, { updateLog, clearCurrent })(
+  EditLogModal
+);
