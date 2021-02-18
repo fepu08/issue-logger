fffa2dd9a8c49550cccc483b04b708517e5bab42
fepu08 <ferencbesenyei@protonmail.com>
2021-02-18 10:44:24 +0100

    techs reducer, action, component

---
+++ src/reducers/techReducer.js
@@ -0,0 +1,31 @@
+import { GET_TECHS, SET_LOADING, TECH_ERROR } from '../actions/types';
+
+const initialState = {
+  techs: null,
+  loading: false,
+  error: null
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GET_TECHS:
+      return {
+        ...state,
+        techs: action.payload,
+        loading: false
+      };
+    case TECH_ERROR:
+      console.error(action.payload);
+      return {
+        ...state,
+        error: action.payload
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+    default:
+      return state;
+  }
+};
